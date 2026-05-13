1f192a618558f7001fe9daa2edd39b33a895c9a8
Tdibacco17 <tomasdibacco@gmail.com>
2026-05-13 14:53:15 -0300

    fix

--- src/components/home/HomeProjects.tsx
+++ src/components/home/HomeProjects.tsx
@@ -56,7 +56,7 @@ export function HomeProjects({
 
   return (
     <div
-      className="relative mt-[calc(var(--vh,1svh))]"
+      className="relative mt-[calc(var(--vh,1vh)*-100)]"
       style={{
         height: wrapperHeight ? `${wrapperHeight}px` : "300vh",
       }}
